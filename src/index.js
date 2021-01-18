3ce0afde1dedf84447b06ff1f7280ad1c5c1574b
Efunkunle Olalekan <efunkunleolalekan@gmail.com>
2021-01-18 05:59:07 +0100

    error handlingt

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 3003
 app.get('/api/rates', async (req, res)=>{
  const base = req.query.base;
  const rates = req.query.currency;
-if(!base||!rates) return res.send('please input a base currency and at least one currency you want to check against')
+if(!base||!rates) return res.send({err:'please input a base currency and at least one currency you want to check against'})
   try{
     const result = await getRates(base, rates)
-    result.error? res.status(400).send(result): res.status(200).send(result)
+    result.error? res.status(400).send({err:'currency code may not be valid', result}): res.status(200).send(result)
   }catch(err){
       res.status(501).send(err)
   }
